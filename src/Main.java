492295c9eb3bb8a47071d844beb9aa4ea775c8e2
Marker97 <chereche10@gmail.com>
2022-11-14 15:02:36 +0100

    Fix: JavaDoc_01

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,12 @@
 
 public class Main {
     public static void main(String[] args){
-
-
-        /**
-         * Using the class "WordGames" , create a new object
-         */
         WordGames wordGames = new WordGames();
         /**
-         * using the object created, call back the two methods of WordGames
-         * assign values to the param of the two methods
+         * assign values to the param and print the two method of WordGames class
          */
-        wordGames.addHelloWord("Hello Word");
-        wordGames.getFullName("Marcus", "Marziano");
+        System.out.println(wordGames.addHelloWord("Marcus"));
+        System.out.println(wordGames.getFullName("Marcus", "Marziano"));
 
     }
 }
